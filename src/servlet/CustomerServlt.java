ab620c56e18b99cd7e97281749dfeed59a144cc4
HelithaSri <helitha.pravin@gmail.com>
2022-05-15 16:19:38 +0530

    Customer Added Done

--- src/servlet/CustomerServlt.java
+++ src/servlet/CustomerServlt.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import javax.annotation.Resource;
+import javax.json.Json;
+import javax.json.JsonObjectBuilder;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -25,26 +28,65 @@ public class CustomerServlt extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+
         String cusID = req.getParameter("cusID");
         String cusName = req.getParameter("cusName");
         String cusAddress = req.getParameter("cusAddress");
         String cusSalary = req.getParameter("cusSalary");
+        PrintWriter writer = resp.getWriter();
 
+        Connection connection = null;
+        try {
+            connection = ds.getConnection();
+            PreparedStatement pstm = connection.prepareStatement("INSERT INTO customer values(?,?,?,?)");
+            pstm.setObject(1, cusID);
+            pstm.setObject(2, cusName);
+            pstm.setObject(3, cusAddress);
+            pstm.setObject(4, cusSalary);
+
+            if (pstm.executeUpdate() > 0) {
+                JsonObjectBuilder response = Json.createObjectBuilder();
+                resp.setStatus(HttpServletResponse.SC_CREATED);//201
+                response.add("status", 200);
+                response.add("message", "Successfully Added");
+                response.add("data", "");
+                writer.print(response.build());
+            }
+
+        } catch (SQLException e) {
+            JsonObjectBuilder response = Json.createObjectBuilder();
+            response.add("status", 400);
+            response.add("message", "Error");
+            response.add("data", e.getLocalizedMessage());
+            writer.print(response.build());
+            resp.setStatus(HttpServletResponse.SC_OK); //200
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
             Connection connection = ds.getConnection();
-            PreparedStatement pstm = connection.prepareStatement("INSERT INTO customer values(?,?,?,?)");
-            pstm.setObject(1,cusID);
-            pstm.setObject(2,cusName);
-            pstm.setObject(3,cusAddress);
-            pstm.setObject(4,cusSalary);
+            String option = req.getParameter("option");
+            resp.setContentType("application/json");
 
-            if (pstm.executeUpdate()>0){
-                System.out.println("cus done");
+            switch (option){
+                case "SEARCH":
+
+                case "GETALL":
 
             }
-            connection.close();
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
+
 }
