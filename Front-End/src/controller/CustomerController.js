74e7b47b181d9414f41841bcf9918fe8e3bc4c72
HelithaSri <helitha.pravin@gmail.com>
2022-05-16 17:28:33 +0530

    The code in 'CustomerController.js' has been restructured

--- Front-End/src/controller/CustomerController.js
+++ Front-End/src/controller/CustomerController.js
@@ -1,46 +1,172 @@
 $("#btnAddCus").prop('disabled', true);
-var clickedRowCId;
-/* Validation - Start */
-$('#error1').css({"display": "none"});
-$('#error2').css({"display": "none"});
-$('#error3').css({"display": "none"});
-$('#error4').css({"display": "none"});
+let clickedRowCId;
 
-$('#error01').css({"display": "none"});
-$('#error02').css({"display": "none"});
-$('#error03').css({"display": "none"});
-$('#error04').css({"display": "none"});
+//Customer Btn Click On Home Page
+$("#customer-clicks").click(function () {
+    validationStaff() // Staff required for validation
+    loadAllCustomers(); //load all customers
+    clearFields()   //Clear Input Fields
+    disableEdit();  //Prevent Editing Customer ID
+})
 
-// var regExCusID = /^(C00-)[0-9]{3,4}$/;
-var RegExCusName = /^[A-z ]{5,20}$/;
-var RegExCusAddress = /^[0-9/A-z. ,]{7,}$/;
-var RegExCusSalary = /^[0-9]{1,}[.]?[0-9]{1,2}$/;
-
-/* Functions Call Section - Start */
-
-// Customer Validation Function Call - Start
-// validation(regExCusID, '#cusIdAdd', '#error1', '#cusNameAdd', '#btnAddCus');
-validation(RegExCusName, '#cusNameAdd', '#error2', '#cusAddressAdd', '#btnAddCus');
-validation(RegExCusAddress, '#cusAddressAdd', '#error3', '#cusSalaryAdd', '#btnAddCus');
-validation(RegExCusSalary, '#cusSalaryAdd', '#error4', "#btnAddCus", '#btnAddCus');
-
-// validation(regExCusID, '#cusIdUpdate', '#error01', "#cusNameUpdate", '#btnUpdateCus');
-validation(RegExCusName, '#cusNameUpdate', '#error02', '#cusAddressUpdate', '#btnUpdateCus');
-validation(RegExCusAddress, '#cusAddressUpdate', '#error03', '#cusSalaryUpdate', '#btnUpdateCus');
-validation(RegExCusSalary, '#cusSalaryUpdate', '#error04', '#btnUpdateCus', '#btnUpdateCus');
-
-generateId();   //Generate New Customer ID
-addCustomer(); //Add New Customer
-loadAllCustomers(); //load all customers
-clearSearch(); //Clear Search and Refresh table
-disableEdit();  //Prevent Editing Customer ID
-// deleteCustomer();
-
-/* Functions Call Section - End */
-
+//Add Customer Outside Btn Click
 $("#addCusModelPop").click(function () {
     generateId();
 });
+
+//Add Customer Btn Click
+$("#btnAddCus").click(function () {
+    addCustomer();
+});
+
+//Clear Search Btn Click
+$("#clear-btn-cus").click(function () {
+    loadAllCustomers(); //load all customers
+    clearFields()   //Clear Input Fields
+});
+
+//Search Btn Click
+$("#button-cus-search").click(function () {
+    if (!$("#txt-cus-search").val()) {
+        loadAllCustomers();
+        return;
+    }
+
+    let btns = "<button class='btn btn-warning' data-bs-target='#updateCustomer' data-bs-toggle='modal'><i class='bi bi-arrow-clockwise'></i></button> <button class='btn btn-danger cus-delete'><i class='bi bi-trash'></i></button>";
+    $.ajax({
+        url: "http://localhost:8080/pos/customer?option=SEARCH", method: "GET", data: {
+            id: $("#txt-cus-search").val()
+        }, success: function (resp) {
+            if (resp.status == 200) {
+                $("#cusTblBody").empty();
+                for (const customer of resp.data) {
+                    let row = `<tr><td>${customer.id}</td><td>${customer.name}</td><td>${customer.address}</td><td>${customer.salary}</td><td>${btns}</td></tr>`;
+                    $("#cusTblBody").append(row);
+                    bindCustomerRow();
+                    deleteCustomer();
+                }
+            } else {
+                alert(resp.data);
+                loadAllCustomers(); //load all customers
+                clearFields()   //Clear Input Fields
+            }
+        }
+    });
+});
+
+//Generate Customer ID
+function generateId() {
+    $.ajax({
+        url: "http://localhost:8080/pos/customer?option=GENERATED_ID", method: "GET", success: function (resp) {
+            if (resp.status == 200) {
+                $("#cusIdAdd").val(resp.data.id);
+            } else {
+                alert(resp.data);
+            }
+        }
+    });
+}
+
+// Customer Add Function - Start
+function addCustomer() {
+    $.ajax({
+        url: "http://localhost:8080/pos/customer",
+        method: "POST",
+        data: $("#addCusForm").serialize(),
+        success: function (res) {
+            if (res.status == 200) {
+                // alert(res.message);
+                generateId();
+                loadAllCustomers();
+                clearFields()   //Clear Input Fields
+                loadAllCustomerIds();   //Load Customer ID's to Combo Box
+                $("#addCustomer").modal('hide');
+            } else {
+                alert(res.data);
+            }
+
+        },
+        error: function (ob, textStatus, error) {
+            console.log(ob);
+            console.log(textStatus);
+            console.log(error);
+        }
+    });
+}
+
+//Update Customer Btn Click
+$("#btnUpdateCus").click(function () {
+    var cusOb = {
+        id: $("#cusIdUpdate").val(),
+        name: $("#cusNameUpdate").val(),
+        address: $("#cusAddressUpdate").val(),
+        salary: $("#cusSalaryUpdate").val()
+    }
+
+    $.ajax({
+        url: "http://localhost:8080/pos/customer", method: "PUT", // contentType: "application/json",
+        data: JSON.stringify(cusOb), success: function (resp) {
+            if (resp.status == 200) {
+                loadAllCustomers();
+                clearFields()   //Clear Input Fields
+                $("#updateCustomer").modal('hide');
+            } else if (resp.status == 400) {
+                alert(resp.data);
+            }
+        }
+    })
+});
+
+// Load All Customers Function - Start
+function loadAllCustomers() {
+    let btns = "<button class='btn btn-warning' data-bs-target='#updateCustomer' data-bs-toggle='modal'><i class='bi bi-arrow-clockwise'></i></button> <button class='btn btn-danger cus-delete'><i class='bi bi-trash'></i></button>";
+    $("#cusTblBody").empty(); //Duplicate Old rows remove
+    $.ajax({
+        url: "http://localhost:8080/pos/customer?option=GETALL", method: "GET", success: function (resp) {
+            for (const customer of resp.data) {
+                let row = `<tr><td>${customer.id}</td><td>${customer.name}</td><td>${customer.address}</td><td>${customer.salary}</td><td>${btns}</td></tr>`;
+                $("#cusTblBody").append(row);
+                bindCustomerRow();
+                deleteCustomer();
+            }
+        }
+    });
+}
+
+// Bind Events Customer Row Function - Start
+function bindCustomerRow() {
+    $("#cusTblBody>tr").click(function () {
+        clickedRowCId = $(this).children(":eq(0)").text();
+        let custName = $(this).children(":eq(1)").text();
+        let custAddress = $(this).children(":eq(2)").text();
+        let custSalary = $(this).children(":eq(3)").text();
+
+        $("#cusIdUpdate").val(clickedRowCId);
+        $("#cusNameUpdate").val(custName);
+        $("#cusAddressUpdate").val(custAddress);
+        $("#cusSalaryUpdate").val(custSalary);
+    });
+}
+
+//Delete Customer Function - Start
+function deleteCustomer() {
+    $(".cus-delete").click(function () {
+        console.log(clickedRowCId);
+        var s = "C00-001";
+        $.ajax({
+            url: `http://localhost:8080/pos/customer?customerID=${clickedRowCId}`,
+            method: "DELETE",
+            success: function (resp) {
+                if (resp.status == 200) {
+                    loadAllCustomers();
+                    clearFields()   //Clear Input Fields
+                } else if (resp.status == 400) {
+                    alert(resp.data);
+                }
+            }
+        });
+    });
+}
 
 // Customer Validation Function - Start
 function validation(regEx, id, error, nextId, btn) {
@@ -61,281 +187,40 @@ function validation(regEx, id, error, nextId, btn) {
     });
 }
 
-// Customer Validation Function - End
-
-// Customer Add Function - Start
-function addCustomer() {
-
-    $("#btnAddCus").click(function () {
-        // console.log($("#addCusForm").serialize());
-        $.ajax({
-            url: "http://localhost:8080/pos/customer",
-            method: "POST",
-            data: $("#addCusForm").serialize(),
-            success: function (res) {
-                if (res.status == 200) {
-                    // alert(res.message);
-                    generateId();
-                    loadAllCustomers();
-                    clearFields()   //Clear Input Fields
-                    loadAllCustomerIds();   //Load Customer ID's to Combo Box
-                    $("#addCustomer").modal('hide');
-                } else {
-                    alert(res.data);
-                }
-
-            },
-            error: function (ob, textStatus, error) {
-                console.log(ob);
-                console.log(textStatus);
-                console.log(error);
-            }
-        });
-        /* let custId = $("#cusIdAdd").val();
-        let custName = $("#cusNameAdd").val();
-        let custAddress = $("#cusAddressAdd").val();
-        let custSalary = $("#cusSalaryAdd").val();
-        let btns =
-            "<button class='btn btn-warning' data-bs-target='#updateCustomer' data-bs-toggle='modal'><i class='bi bi-arrow-clockwise'></i></button> <button class='btn btn-danger cus-delete'><i class='bi bi-trash'></i></button>";
-
-        var customerObj = new CustomerDTO(custId, custName, custAddress, custSalary, btns);
-        customerDB.push(customerObj); */
-        // loadAllCustomers(); //load all customers
-
-    });
-}
-
-// Customer Add Function - End
-
-
-// Load All Customers Function - Start
-function loadAllCustomers() {
-    let btns = "<button class='btn btn-warning' data-bs-target='#updateCustomer' data-bs-toggle='modal'><i class='bi bi-arrow-clockwise'></i></button> <button class='btn btn-danger cus-delete'><i class='bi bi-trash'></i></button>";
-    $("#cusTblBody").empty(); //Duplicate Old rows remove
-    $.ajax({
-        url: "http://localhost:8080/pos/customer?option=GETALL", method: "GET", success: function (resp) {
-            for (const customer of resp.data) {
-                let row = `<tr><td>${customer.id}</td><td>${customer.name}</td><td>${customer.address}</td><td>${customer.salary}</td><td>${btns}</td></tr>`;
-                $("#cusTblBody").append(row);
-                bindCustomerRow();
-                deleteCustomer();
-                // console.log(customer)
-            }
-        }
-    });
-
-    /*$("#cusTblBody").empty(); //Duplicate Old rows remove
-    for (let i = 0; i < customerDB.length; i++) {
-        let nRow =
-            "<tr><td>" +
-            customerDB[i].getCustomerID() +
-            "</td><td>" +
-            customerDB[i].getCustomerName() +
-            "</td><td>" +
-            customerDB[i].getCustomerAddress() +
-            "</td><td>" +
-            customerDB[i].getCustomerSalary() +
-            "</td><td class='text-center'>" +
-            customerDB[i].getCustomerbtn() +
-            "</td></tr>";
-
-        $("#cusTblBody").append(nRow);
-        bindCustomerRow();
-        deleteCustomer();
-    }*/
-}
-
-// Load All Customers Function - End
-
-
-// Bind Events Customer Row Function - Start
-function bindCustomerRow() {
-    $("#cusTblBody>tr").click(function () {
-
-        clickedRowCId = $(this).children(":eq(0)").text();
-        let custName = $(this).children(":eq(1)").text();
-        let custAddress = $(this).children(":eq(2)").text();
-        let custSalary = $(this).children(":eq(3)").text();
-
-        $("#cusIdUpdate").val(clickedRowCId);
-        $("#cusNameUpdate").val(custName);
-        $("#cusAddressUpdate").val(custAddress);
-        $("#cusSalaryUpdate").val(custSalary);
-    });
-}
-
-// Bind Events Customer Row - End
-
-$("#button-cus-search").click(function () {
-    if (!$("#txt-cus-search").val()) {
-        loadAllCustomers();
-        return;
-    }
-
-    var cusOb = {
-        id: $("#txt-cus-search").val()
-    }
-
-    let btns = "<button class='btn btn-warning' data-bs-target='#updateCustomer' data-bs-toggle='modal'><i class='bi bi-arrow-clockwise'></i></button> <button class='btn btn-danger cus-delete'><i class='bi bi-trash'></i></button>";
-    $.ajax({
-        url: "http://localhost:8080/pos/customer?option=SEARCH",
-        method: "GET",
-        data: {
-            id: $("#txt-cus-search").val()
-        },
-        success: function (resp) {
-            if (resp.status == 200) {
-                $("#cusTblBody").empty();
-                for (const customer of resp.data) {
-                    let row = `<tr><td>${customer.id}</td><td>${customer.name}</td><td>${customer.address}</td><td>${customer.salary}</td><td>${btns}</td></tr>`;
-                    $("#cusTblBody").append(row);
-                    bindCustomerRow();
-                    deleteCustomer();
-                }
-            } else {
-                alert(resp.data);
-                clearSearch(); //Clear Search and Refresh table
-            }
-        }
-    });
-
-    /*var searchId = $("#txt-cus-search").val();
-    var response = searchCustomer(searchId);
-    if (response) {
-        $("#cusTblBody").empty();
-        let nRow = "<tr><td>" + response.getCustomerID() + "</td><td>" + response.getCustomerName() + "</td><td>" + response.getCustomerAddress() + "</td><td>" + response.getCustomerSalary() + "</td><td class='text-center'>" + response.getCustomerbtn() + "</td></tr>";
-        $("#cusTblBody").append(nRow);
-        bindCustomerRow();
-        deleteCustomer();
-    } else {
-        alert("No Such a customer");
-        clearSearch(); //Clear Search and Refresh table
-    }*/
-});
-
-function searchCustomer(id) {
-    for (let i = 0; i < customerDB.length; i++) {
-        if (customerDB[i].getCustomerID() == id) {
-            return customerDB[i];
-        }
-    }
-}
-
-//clear search function - start
-function clearSearch() {
-    $("#clear-btn-cus").click(function () {
-        loadAllCustomers(); //load all customers
-        clearFields()   //Clear Input Fields
-    });
-}
-
-//clear search function - End
-
-//Delete Customer Function - Start
-function deleteCustomer() {
-    $(".cus-delete").click(function () {
-        /*for (let i = 0; i < customerDB.length; i++) {
-            // console.log(customerDB[i].getCustomerID());
-            if (customerDB[i].getCustomerID() == clickedRowCId) {
-                customerDB.splice(i, 1);
-            }
-        }
-        loadAllCustomers();
-        clearFields()   //Clear Input Fields*/
-        console.log(clickedRowCId);
-        $.ajax({
-            url: `http://localhost:8080/pos/customer?customerID=${clickedRowCId}`,
-            method: "DELETE",
-            success: function (resp) {
-                if (resp.status == 200) {
-                    loadAllCustomers();
-                    clearFields()   //Clear Input Fields
-                } else if (resp.status == 400) {
-                    alert(resp.data);
-                }
-            }
-        });
-
-    });
-}
-
-//Delete Customer Function - End
-
-$("#btnUpdateCus").click(function () {
-
-    var cusOb = {
-        id: $("#cusIdUpdate").val(),
-        name: $("#cusNameUpdate").val(),
-        address: $("#cusAddressUpdate").val(),
-        salary: $("#cusSalaryUpdate").val()
-    }
-
-    console.log(typeof cusOb)
-    console.log(cusOb)
-    $.ajax({
-        url: "http://localhost:8080/pos/customer", method: "PUT", // contentType: "application/json",
-        data: JSON.stringify(cusOb), success: function (resp) {
-            if (resp.status == 200) {
-                loadAllCustomers();
-                clearFields()   //Clear Input Fields
-                $("#updateCustomer").modal('hide');
-            } else if (resp.status == 400) {
-                alert(resp.data);
-            }
-        }
-    })
-
-    /*let custId = $("#cusIdUpdate").val();
-    let custName = $("#cusNameUpdate").val();
-    let custAddress = $("#cusAddressUpdate").val();
-    let custSalary = $("#cusSalaryUpdate").val();
-
-    for (let i = 0; i < customerDB.length; i++) {
-        if (customerDB[i].getCustomerID() == custId) {
-            customerDB[i].setCustomerName(custName);
-            customerDB[i].setCustomerAddress(custAddress);
-            customerDB[i].setCustomerSalary(custSalary);
-        }
-    }*/
-
-});
-
-function generateId() {
-    $.ajax({
-        url: "http://localhost:8080/pos/customer?option=GENERATED_ID", method: "GET", success: function (resp) {
-            if (resp.status == 200) {
-                $("#cusIdAdd").val(resp.data.id);
-            } else {
-                alert(resp.data);
-            }
-        }
-    });
-    /*let index = customerDB.length - 1;
-    let id;
-    let temp;
-    if (index != -1) {
-        id = customerDB[customerDB.length - 1].getCustomerID();
-        temp = id.split("-")[1];
-        temp++;
-    }
-
-    if (index == -1) {
-        $("#cusIdAdd").val("C00-001");
-    } else if (temp <= 9) {
-        $("#cusIdAdd").val("C00-00" + temp);
-    } else if (temp <= 99) {
-        $("#cusIdAdd").val("C00-0" + temp);
-    } else {
-        $("#cusIdAdd").val("C00-" + temp);
-    }*/
-}
-
+//Prevent Edit Customer ID Input Fields
 function disableEdit() {
     $("#cusIdAdd,#cusIdUpdate").css("pointer-events", "none");
 }
 
+//Clear Input All Fields
 function clearFields() {
     $("#cusNameAdd,#cusAddressAdd,#cusSalaryAdd").val("");    // Clear input Fields (Add)
     $("#cusNameUpdate,#cusAddressUpdate,#cusSalaryUpdate").val(""); // Clear input Fields (Update)
     $("#txt-cus-search").val(""); //Clear input Field (Search)
+}
+
+// Staff required for validation
+function validationStaff() {
+    var RegExCusName = /^[A-z ]{5,20}$/;
+    var RegExCusAddress = /^[0-9/A-z. ,]{7,}$/;
+    var RegExCusSalary = /^[0-9]{1,}[.]?[0-9]{1,2}$/;
+
+    $('#error1').css({"display": "none"});
+    $('#error2').css({"display": "none"});
+    $('#error3').css({"display": "none"});
+    $('#error4').css({"display": "none"});
+
+    $('#error01').css({"display": "none"});
+    $('#error02').css({"display": "none"});
+    $('#error03').css({"display": "none"});
+    $('#error04').css({"display": "none"});
+
+    // Customer Validation Function Call - Start
+    validation(RegExCusName, '#cusNameAdd', '#error2', '#cusAddressAdd', '#btnAddCus');
+    validation(RegExCusAddress, '#cusAddressAdd', '#error3', '#cusSalaryAdd', '#btnAddCus');
+    validation(RegExCusSalary, '#cusSalaryAdd', '#error4', "#btnAddCus", '#btnAddCus');
+
+    validation(RegExCusName, '#cusNameUpdate', '#error02', '#cusAddressUpdate', '#btnUpdateCus');
+    validation(RegExCusAddress, '#cusAddressUpdate', '#error03', '#cusSalaryUpdate', '#btnUpdateCus');
+    validation(RegExCusSalary, '#cusSalaryUpdate', '#error04', '#btnUpdateCus', '#btnUpdateCus');
 }
