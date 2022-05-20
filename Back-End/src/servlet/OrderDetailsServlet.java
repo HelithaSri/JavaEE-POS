ea949d226aac35b702eaedd4ee156333f03615b7
HelithaSri <helitha.pravin@gmail.com>
2022-05-21 03:27:09 +0530

    Order Details Done

---
+++ Back-End/src/servlet/OrderDetailsServlet.java
@@ -0,0 +1,90 @@
+package servlet;
+
+import javax.annotation.Resource;
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * @author Helitha Sri
+ * @created 5/21/2022 - 1:02 AM
+ * @project JavaEE POS Backend
+ */
+
+@WebServlet(urlPatterns = "/oDetails")
+public class OrderDetailsServlet extends HttpServlet {
+    @Resource(name = "java:comp/env/jdbc/pool")
+    DataSource ds;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+        JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+        JsonObjectBuilder dataMsgBuilder = Json.createObjectBuilder();
+        PrintWriter writer = resp.getWriter();
+
+        String orderID = req.getParameter("orderID");
+        Connection connection = null;
+        try {
+            connection = ds.getConnection();
+            PreparedStatement pstm = connection.prepareStatement("SELECT o.customerID, o.date, o.subTotal, od.*, i.description FROM orders o, orderdetails od, item i WHERE (o.oid =od.oid AND od.itemCode=CODE) AND CONCAT(o.oid) LIKE ?");
+            pstm.setObject(1,"%"+orderID+"%");
+            ResultSet rst = pstm.executeQuery();
+            resp.setStatus(HttpServletResponse.SC_OK);//201
+
+            while (rst.next()){
+                String cusiD = rst.getString(1);
+                String date = rst.getString(2);
+                String subTotal = rst.getString(3);
+                String oID = rst.getString(4);
+                String itemCode = rst.getString(5);
+                String qty = rst.getString(6);
+                String unitPrice = rst.getString(7);
+                String total = rst.getString(8);
+                String itemName = rst.getString(9);
+
+                objectBuilder.add("cusID", cusiD);
+                objectBuilder.add("date", date);
+                objectBuilder.add("subTotal", subTotal);
+                objectBuilder.add("oID", oID);
+                objectBuilder.add("itemCode", itemCode);
+                objectBuilder.add("qty", qty);
+                objectBuilder.add("unitPrice", unitPrice);
+                objectBuilder.add("total", total);
+                objectBuilder.add("itemName", itemName);
+                arrayBuilder.add(objectBuilder.build());
+            }
+            dataMsgBuilder.add("data", arrayBuilder.build());
+            dataMsgBuilder.add("massage", "Done");
+            dataMsgBuilder.add("status", 200);
+            writer.print(dataMsgBuilder.build());
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            dataMsgBuilder.add("data", arrayBuilder.build());
+            dataMsgBuilder.add("massage", "Error");
+            dataMsgBuilder.add("status", 400);
+            writer.print(dataMsgBuilder.build());
+        }finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
--- Front-End/src/controller/OrderDetailsController.js
+++ Front-End/src/controller/OrderDetailsController.js
@@ -15,14 +15,37 @@ $("#clear-btn-order").click(function () {
 });
 
 function searchOrderDetails(result) {
+    let oid = $("#txt-order-search").val();
+    $.ajax({
+        url:`http://localhost:8080/pos/oDetails?orderID=${oid}`,
+        method:"GET",
+        success:function (resp) {
+            if (resp.status == 200) {
+                $("#orderDetailsTblBody").empty();
+                for (const fetch of resp.data) {
+                    let row = `<tr><td>${fetch.itemCode}</td><td>${fetch.itemName}</td><td>${fetch.unitPrice}</td><td>${fetch.qty}</td><td>${fetch.total}</td></tr>`;
+                    $("#orderDetailsTblBody").append(row);
+                    bindCustomerRow();
 
-    for (const od of orderDetailsDB) {
+                    $("#lblOrderId").text(fetch.oID);
+                    $("#lblCusId").text(fetch.cusID);
+                    $("#lblDate").text(fetch.date);
+                    $("#lblSTotal").text(fetch.subTotal);
+                }
+            } else {
+                alert(resp.data)
+            }
+        }
+    })
+
+    /*for (const od of orderDetailsDB) {
         if (od.getOrderid() == result) {
 
             $("#lblOrderId").text(od.getOrderid());
 
             for (const o of orderDB) {
                 if (o.getOrderId() == result) {
+                    $("#lblOrderId").text(od.getOrderid());
                     $("#lblCusId").text(o.getCustomerId());
                     $("#lblDate").text(o.getDate());
                     $("#lblSTotal").text(o.getSubTotal());
@@ -34,5 +57,5 @@ function searchOrderDetails(result) {
             $("#orderDetailsTblBody").append(row);
 
         }
-    }
+    }*/
 }
